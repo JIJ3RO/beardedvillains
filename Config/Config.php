003a81844ae540e0921220498e9921f45d0ff510
JIJ3RO <Alexis.dominguez.isw@unipolidgo.edu.mx>
2025-01-15 20:57:25 -0600

    primer repositorio

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tienda-online/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-youtube";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AcjgcaZs72GozO573J9jrdF5x0aJ5krtMFHzb5RZYsbODXvRIr0DtII3SqoVi8UU7WsWNnBmBEjiJeoN";
+
+const USER_SMTP = "alexis.dominguez.isw@unipolidgo.edu.mx";
+const PASS_SMTP = "wnoa nsux hfeo nhzv";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
